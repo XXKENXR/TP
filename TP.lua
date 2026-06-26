dfa21dc7cacbb9023a593f5546ae26f65cc69e7c
XXKENXR <kr165675@gmail.com>
2026-06-26 07:02:59 -0400

    Actualizar TP.lua

--- TP.lua
+++ TP.lua
@@ -1,9 +1,9 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "*Kenscript* 🍫+1 Escapa del teclado🍩",
+   Name = "*Kenscript* 🍫 +1 Escapa del teclado 🍩",
    LoadingTitle = "Cargando Kenscript...",
-   LoadingSubtitle = "by xxkenxr🇵🇷",
+   LoadingSubtitle = "by xxkenxr 🇵🇷",
    ConfigurationSaving = { Enabled = false },
 })
 
